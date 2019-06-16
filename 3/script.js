fd85e9d331a6048e2612697714ff98185cc13e91
AlekseiKromski <aleksei.kromski@ivkhk.ee>
2019-06-16 15:52:06 +0300

    Начало обучения JS

---
+++ 3/script.js
@@ -0,0 +1,7 @@
+var num1 = 12;
+var num2 = 10;
+
+//console.log( '+ = ' , num1 + num2 ); // операция сложений 
+//console.log( '- = ' , num1 - num2 ); // операция вычисления 
+console.log( '* = ' , num1 * num2 ); // операция умножения 
+console.log( '/ = ' , num1 / num2 ); // операция деления 
