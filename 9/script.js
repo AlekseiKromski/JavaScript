640a93e0f6a1b3fbb467d863e2ff09659f4de3c2
AlekseiKromski <aleksei.kromski@ivkhk.ee>
2019-06-22 19:32:25 +0300

    switch case

--- 9/script.js
+++ 9/script.js
@@ -1,5 +1,33 @@
-//Тернарные операторы 
-//Нужны для упрощения кода
-var personAge = 20;
-var message = personAge < 18 ? 'Человек не совершеннолетний' : 'Человек совершеннолетний' ;
-console.log(message);
+// Switch case
+
+var carColor = 'red';
+
+/*
+
+if ( carColor === 'green' ){
+    console.log('Зеленая');
+} else if ( carColor === 'yellow' ){
+    console.log('Желтая');
+} else if ( carColor === 'red' ){
+    console.log('Красная');
+} else{
+    console.log('Цвет машины не определен');
+}
+*/
+
+// Упрощение 
+
+switch (carColor){ //Все время проверяет одну переменную 
+    case 'green':
+        console.log('Зеленая');
+        break
+    case 'yellow':
+        console.log('Желтая');
+        break
+    case 'red':
+        console.log('Красная');
+        break
+    default: // аналог else
+        console.log('Цвет машины не определен');
+}
+// break нужен для того, чтобы код в default не выполнился, если case условие было true 
