e76901ee5c65c9dda41001df8f6a1bd439b34e28
aleksei.kromski <aleksei.kromski@ivkhk.ee>
2019-08-25 23:58:26 +0300

    Приоритет операций

---
+++ 4/script.js
@@ -0,0 +1,7 @@
+var result = 12 - 6 / 3
+var result2 =  3 + 4 * 8
+console.log('result =',result)
+console.log('result =',result2)
+
+var isGrater = 20 - 6 * 3 >= 4
+console.log('result =',isGrater)
