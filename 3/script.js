a5259e9124931134fd898d46c26ebbf9ea170e93
aleksei.kromski <aleksei.kromski@ivkhk.ee>
2019-08-25 22:36:30 +0300

    Базовые операции

---
+++ 3/script.js
@@ -0,0 +1,23 @@
+var num1 = 12
+var num2 = 8
+
+
+console.log('+ = ', num1 + num2)
+console.log('- = ', num1 - num2)
+console.log('* = ', num1 * num2)
+console.log('/ = ', num1 / num2)
+console.log('** = ', num1 ** num2)
+
+
+var str1 = 'Hellow'
+var str2 = 'Zumma'
+
+console.log(' + = ', str1 + ', ' +str2)
+
+console.log(' 1 + 2 = ', 1 + '2')
+console.log(' str1 + num1 = ', str1 + num1)
+
+console.log(' boolean + str = ', true + str2)
+
+console.log('true + 1 = ', true + 1) // true = 1 ( 1 + 1 = 2  )
+console.log('false + 1 = ', false + 1) // false = 0 ( 0 + 1 = 1 )
