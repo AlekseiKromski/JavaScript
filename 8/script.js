4e756abce2128a6cece131bd89ed6b0c13d1afa9
aleksei.kromski <aleksei.kromski@ivkhk.ee>
2019-08-31 20:50:47 +0300

    Praktika

--- 8/script.js
+++ 8/script.js
@@ -23,3 +23,22 @@ switch (carColor){
     default:
         console.log('Цвет машины не определен')    
 }
+
+var CarName = 'Sidan'
+var CarAge = 2019
+var NowDate = 2019
+
+var CarYear = NowDate - CarAge
+
+console.log("Машине - ", CarYear)
+
+switch(true){
+    case (CarYear < 5):
+        console.log("Ваша машина меньше 5 лет")
+        break
+    case (CarYear > 10):
+        console.log("Ваша машина больше 10 лет")
+        break
+    default:
+        console.log("Сбой проверки")
+}
