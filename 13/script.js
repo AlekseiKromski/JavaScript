9d463470dcb3f6f10861de13b797b99b3ec7819e
aleksei.kromski <aleksei.kromski@ivkhk.ee>
2019-09-12 17:51:36 +0300

    Объекты

---
+++ 13/script.js
@@ -0,0 +1,10 @@
+var person = {
+    name: 'Vasili',
+    year: 1990,
+    family: ['Jelena', 'Igor'],
+    car: {
+        year: 2010,
+        model: 'ford'
+    }
+}
+console.log(person.car.model)
