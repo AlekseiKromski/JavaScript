e2f57bcf439bfa46d72c163c5d04d380cdd0318b
aleksei.kromski <aleksei.kromski@ivkhk.ee>
2019-09-12 18:19:57 +0300

    Доступ к элементам

---
+++ script.js
@@ -0,0 +1,10 @@
+/*
+var div = document.getElementById('playground')
+var p = document.getElementsByClassName('text')
+var h1 = document.getElementsByTagName('h1')
+console.log(h1)
+
+*/ 
+
+var div = document.querySelector('#playground')
+console.log(div)
