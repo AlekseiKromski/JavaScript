c5e6c4b788dbfeb4be1dc3bcc7b3eb881380d069
AlekseiKrosmki <aleksei.kromski@ivkhk.ee>
2020-01-22 16:59:19 +0200

    Содержимое элементов

--- script.js
+++ script.js
@@ -15,9 +15,14 @@ var div = document.querySelector('#playground')
 var p = document.querySelectorAll('.text')
 var h1 = document.querySelector('h1')
 div.innerHTML = '<h2 style="color:red;">Hellow from JS</h2>'
+h1.textContent = 'Changed from js'
+var input = document.querySelector('input')
+console.log(input.value);
+
+
 console.log(div.innerHTML);
 console.log(p);
-console.log(h1);
+console.log(h1.textContent);
 
 
 
