9656783ad9b9c0dee9347c7846f930e239818cb4
AlekseiKrosmki <aleksei.kromski@ivkhk.ee>
2020-03-15 13:05:14 +0200

    Содержимое элементов

--- script.js
+++ script.js
@@ -1,125 +1,21 @@
-/*
-//Old version
-var div = document.getElementById('playground')
-var p = document.getElementsByClassName('text')
-var h1 = document.getElementsByTagName('h1')
 
-console.log(div)
-console.log(p)
-console.log(h1)
- 
+var id = document.getElementById('playground');
+console.log(id);
 
-//New version
-//calss -> .CLASS_NAME
-var div = document.querySelector('#playground')
-var p = document.querySelectorAll('.text')
-var h1 = document.querySelector('h1')
-div.innerHTML = '<h2 style="color:red;">Hellow from JS</h2>'
-h1.textContent = 'Changed from js'
-var input = document.querySelector('input')
-
-console.log(input.value);
-console.log(div.innerHTML);
+var p = document.getElementsByClassName('text');
 console.log(p);
+
+var h1 = document.getElementsByTagName('h1');
+console.log(h1);
+
+//Новые методы
+var div = document.querySelector('#playground > ul ');
+console.log(div);
+
+div.innerHTML = '<h2 style="color:red;">Hello from JS</h2>'
+h1.textContent = 'Changed from js'
+console.log(div.innerHTML);
 console.log(h1.textContent);
 
-
-//Атрибуты
-
-
-var a = document.querySelector('a')
-var oldHref = a.getAttribute('href')
-a.setAttribute('href','https://yandex.ru')
-a.textContent = 'Yandex'
-console.log(a.getAttribute('href'))
-
-var box1 = document.querySelector('#box1')
-var box2 = document.querySelector('#box2')
-
-box1.classList.add('red');
-box2.classList.remove('blue');
-var hasClass = box2.classList.contains('box');
-console.log(hasClass)
-
-var button = document.querySelector('button')
-var h1 = document.querySelector('h1')
 var input = document.querySelector('input')
-
-h1.style = 'width:10%; text-align:center;+'
-
-function buttonHandler(){
-    h1.textContent = input.value;
-}
-//Создание события 
-button.addEventListener('click', buttonHandler) //Передаем ссылку на функцию 
-
-h1.addEventListener('mouseenter',function (){
-    this.style.color = 'white'
-    this.style.backgroundColor = 'black'
-})
-
-h1.addEventListener('mouseleave', function(){
-    this.style.color = 'black'
-    this.style.backgroundColor = 'white'
-
-
-})
-
-var divs = document.querySelectorAll('div')
-
-for (var i = 0; i < divs.length; i++){
-    divs[i].addEventListener('click',function(event){
-        event.stopPropagation()
-        console.log(this.getAttribute('id'))
-    })
-}
-
-var a = document.querySelector('a')
-
-a.addEventListener('click', linkClick)
-
-function linkClick(event){
-    event.preventDefault()
-    var div = divs[0]
-    if(div.style.display === 'none'){
-        div.style.display = 'flex'
-    } else{
-        div.style.display = 'none'
-    }
-    console.log(div.style.display) 
-}
-
-var p = document.querySelectorAll('p')
-
-for (var i = 0; i < p.length; i++){
-    p[i].addEventListener('click', function(event){
-        event.target.style.color = 'blue'
-    })
-}
-
-document.getElementById('wrapper').addEventListener('click',function(event){
-    console.log(event.target)
-
-})
-*/
-document.querySelector('#alert').addEventListener('click', function(){
-    alert('Hello, user')
-})
-
-document.querySelector('#confirm').addEventListener('click', function(){
-    var desision = confirm('How are you ?')
-
-    if (desision){
-        alert('Вы кликнули на кнопку')
-    }
-})
-
-document.querySelector('#prompt').addEventListener('click', function(){
-    var age = prompt('Enter your age = ', 0)
-
-    if(age >= 18){
-        alert('Вы можете пройти')
-    }else{
-        alert('Вы молоды')
-    }
-})
+console.log(input.value);
