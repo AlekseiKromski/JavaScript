7b1aac363d9ef48e3a0ea42c7846bbaaa3058ff5
AlekseiKrosmki <aleksei.kromski@ivkhk.ee>
2020-03-18 20:48:49 +0200

    Добавление событий

--- script.js
+++ script.js
@@ -27,7 +27,7 @@ a.setAttribute('href','https://ya.ru')
 a.textContent = 'yandex'
 
 console.log(a.attributes);
-*/
+
 
 var box1 = document.querySelector('#box1')
 var box2 = document.querySelector('#box2')
@@ -45,3 +45,26 @@ if(hasClass){
 } else{
     box2.classList.add('blue')
 }
+*/
+
+var button = document.querySelector('button')
+var h1 = document.querySelector('h1')
+var input = document.querySelector('input')
+
+function buttonHandler(){
+    h1.textContent = input.value
+}
+
+h1.addEventListener('mouseenter',function (){
+    console.log('Mouse has entered h1');
+    console.log(this.style.color = 'red');
+    this.style.color = 'red'
+    
+})
+
+h1.addEventListener('mouseleave',function (){
+    console.log('Mouse has left h1');
+})
+
+button.addEventListener('click',buttonHandler)
+
