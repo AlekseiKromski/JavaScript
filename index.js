ea71f8b2336d8913a4e14de4c091a4a27b754420
AlekseiKrosmki <aleksei.kromski@ivkhk.ee>
2020-03-29 15:19:47 +0300

    Начало игры

---
+++ index.js
@@ -0,0 +1,16 @@
+function startGame(){
+    $start.classList.add('hide')
+    $game.style.backgroundColor = '#fff'
+    renderBox()
+}
+
+
+function renderBox(){
+    
+}
+
+var $start = document.querySelector('#start')
+var $game = document.querySelector('#game')
+
+$start.addEventListener('click',startGame)
+
