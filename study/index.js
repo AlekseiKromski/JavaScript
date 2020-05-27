711657911a4adb5f81c73b0bd4d03a13576b0c28
AlekseiKrosmki <aleksei.kromski@ivkhk.ee>
2020-05-27 11:58:44 +0300

    Асинхронность

--- study/index.js
+++ study/index.js
@@ -1,50 +1,30 @@
-class Dropdown{
-    constructor(selector, options){
-        this.$el = document.querySelector(selector)
-        this.items = options.items
-        this.$el.querySelector(['.dropdown_label']).textContent = this.items[0].label
-        this.$el.addEventListener('click',event => {
-            if(event.target.classList.contains('dropdown_label')){
-                if(this.$el.classList.contains('open')){
-                    this.close()
-                }else{
-                    this.open()
-                }
-            }else if(event.target.tagName.toLowerCase() === 'li'){
-                this.select(event.target.dataset.id);
-            }
-        })
 
-        const itemsHTML = this.items.map(i => {
-            return `<li data-id="${i.id}">${i.label}</li>`
-        }).join(' ')
 
-        this.$el.querySelector('.dropdown_menu').insertAdjacentHTML('afterbegin', itemsHTML)
-    }
 
-    select(id){
-        const item = this.items.find(i => i.id === id)
-        this.$el.querySelector('.dropdown_label').textContent = item.label
-        this.close()
-    }
+function loadAsset(url, type, callback) {
+  let xhr = new XMLHttpRequest();
+  xhr.open('GET', url);
+  xhr.responseType = type;
 
-    open(){
-        this.$el.classList.add('open')
-    }
+  xhr.onload = function() {
+    callback(xhr.response);
+  };
 
-    close(){
-        this.$el.classList.remove('open')
-    }
+  xhr.send();
 }
 
+function displayImage(blob) {
+    var i = 5
+    while(i<10000000){
+        i++
+        console.log(i);
+        
+    }
+  let objectURL = URL.createObjectURL(blob);
 
+  let image = document.createElement('img');
+  image.src = objectURL;
+  document.body.appendChild(image);
+}
 
-
-const dropdown = new Dropdown('#dropdown', {
-    items: [
-      {label: 'Москва', id: 'msk'},
-      {label: 'Санкт-Петербург', id: 'spb'},
-      {label: 'Новосибирск', id: 'nsk'},
-      {label: 'Краснодар', id: 'krdr'}
-    ]
-  })
+loadAsset('https://upload.wikimedia.org/wikipedia/commons/thumb/0/02/Moscow-City_%2836211143494%29.jpg/1200px-Moscow-City_%2836211143494%29.jpg', 'blob', displayImage);
