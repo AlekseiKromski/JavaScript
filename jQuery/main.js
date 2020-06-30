59ba8d882a2353f78d0e111420850aa3c9dc8a27
AlekseiKrosmki <aleksei.kromski@ivkhk.ee>
2020-06-30 21:23:23 +0300

    function for find and select elements in DOM

--- jQuery/main.js
+++ jQuery/main.js
@@ -2,5 +2,5 @@ $(document).ready(function(){
 
     //СЛЕКТОРЫ
     //$('.h1').parent().addClass('selected')
-    $('h1').parents().addClass('selected')
+    $('h1').siblings('h2').addClass('selected')
 });
