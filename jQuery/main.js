806ed663baed292f6a22b5bb9ed94ec2ea0b0ff3
AlekseiKrosmki <aleksei.kromski@ivkhk.ee>
2020-07-01 18:34:27 +0300

    Events in jQuery (some methods)

--- jQuery/main.js
+++ jQuery/main.js
@@ -1,6 +1,20 @@
 $(document).ready(function(){
+   
+    // $('#change_color').on('click', (() => {
+    //     $('.move_block').toggleClass('bg-green')
+    // }));
 
-    //СЛЕКТОРЫ
-    //$('.h1').parent().addClass('selected')
-    $('h1').siblings('h2').addClass('selected')
+
+    $('#change_color').on('dblclick', (() => {
+        $('.move_block').toggleClass('bg-green')
+    }));
+
+    $('.move_block').on('mouseenter', (() => {
+        $('.move_block').css('border', '3px solid orange')
+    }));
+
+    $('#input').on('change', (() => {
+        $('#output_name').text($('#input').val())
+    }));
+    
 });
