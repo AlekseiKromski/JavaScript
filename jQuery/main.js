b4b4d4a2a9cb74c0c8b8efb2dc0d58b60212e692
AlekseiKrosmki <aleksei.kromski@ivkhk.ee>
2020-07-01 20:15:41 +0300

    make first slider

--- jQuery/main.js
+++ jQuery/main.js
@@ -1,13 +1,31 @@
 $(document).ready(function(){
 
+    let next_img = 1;
 
-    $('#submit').on('click', (event) => {
-        event.preventDefault();
-        let v1 = Number($('#input_1').val());
-        let v2 = Number($('#input_2').val());
+    $('#next').on('click', (event) => {
+        event.preventDefault()
+        if(next_img !== 3){
+            let back = $('#img').attr('src');
+            let test = back.indexOf('.jpg')
+            back = Number(back.substr(0,test));
+            next_img = back + 1;
+        }else{
+            next_img = 1;
+        };
+        $('#img').hide(1000)
+        setTimeout(() => {
+            $('#img').attr('src',next_img + '.jpg')
+        },1000)
+        $('#img').show(1000);
         
-        $('#summ').text(v1 + v2);
-    });
+        console.log('Hide');
+        setTimeout(() => {
+            console.log('Change');
+        },1000)
+        console.log('Show');
+        
+        
+    })
 
 
 });
