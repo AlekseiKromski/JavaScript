ce1ced74638d588bea887f463a36681653148488
AlekseiKrosmki <aleksei.kromski@ivkhk.ee>
2020-07-02 20:57:41 +0300

    make to-do-list functional on "clear" js

--- project3/js/dist/main.js
+++ project3/js/dist/main.js
@@ -7,6 +7,7 @@ var button = new _element.Element('#button');
 var input = new _element.Element('#input');
 var prewie_block = new _element.Element('#preview');
 var title = new _element.Element('#title');
+var id = 0;
 
 input.$el.addEventListener('input', function (event) {
     event.preventDefault();
@@ -29,12 +30,26 @@ button.$el.addEventListener('click', function (event) {
     event.preventDefault();
     if (input.$el.value !== '') {
         var block_of_do = new _element.Element('#block-of-do');
-        block_of_do.$el.insertAdjacentHTML('afterbegin', getBlockOfDo(input.$el.value));
+        block_of_do.$el.insertAdjacentHTML('afterbegin', getBlockOfDo(input.$el.value, id));
         prewie_block.$el.style.display = "none";
         input.$el.value = '';
+        var delete_button = new _element.Element('#delete_button');
+        delete_button.$el.addEventListener('click', function (event) {
+            var parent = delete_button.$el.parentNode.parentNode.parentNode.parentNode;
+            var data_attr = delete_button.$el.getAttribute('data-id');
+            for (var i = 0; i < block_of_do.$el.childNodes.length; i++) {
+                if (block_of_do.$el.children[i] !== undefined) {
+                    if (block_of_do.$el.children[i].getAttribute('data-id') === data_attr) {
+                        console.log('YES');
+
+                        block_of_do.$el.children[i].remove();
+                    }
+                }
+            }
+        });
     }
 });
 
 function getBlockOfDo(title) {
-    return '<div class="col-md-12 to-do-block" >\n        <div class="row">\n            <div class="col-11">\n                <p class="m-0" id="title">' + title + '</p>\n            </div>\n            <div class="col-1 p-1">\n                <button class=\'delete-button\'><i class="fa fa-times" aria-hidden="true"></i></button>\n            </div>\n        </div>\n    </div>';
+    return '<div class="col-md-12 to-do-block" data-id=' + ++id + '>\n        <div class="row">\n            <div class="col-11">\n                <p class="m-0" id="title">' + title + '</p>\n            </div>\n            <div class="col-1 p-1">\n                <button class="delete-button" id=\'delete_button\' data-id=' + id + '><i class="fa fa-times" aria-hidden="true"></i></button>\n            </div>\n        </div>\n    </div>';
 }
--- project3/js/src/main.js
+++ project3/js/src/main.js
@@ -5,7 +5,7 @@ const button = new Element('#button');
 const input = new Element('#input');
 const prewie_block = new Element('#preview');
 const title = new Element('#title');
-
+let id = 0;
 
 input.$el.addEventListener('input', function(event){
     event.preventDefault();
@@ -28,21 +28,38 @@ button.$el.addEventListener('click', function(event){
     event.preventDefault();
     if(input.$el.value !== ''){
         const block_of_do = new Element('#block-of-do');
-        block_of_do.$el.insertAdjacentHTML('afterbegin', getBlockOfDo(input.$el.value));
+        block_of_do.$el.insertAdjacentHTML('afterbegin', getBlockOfDo(input.$el.value, id));
         prewie_block.$el.style.display = "none";
         input.$el.value = '';
+        const delete_button = new Element('#delete_button'); 
+        delete_button.$el.addEventListener('click',(event) => {
+            let parent = (((delete_button.$el.parentNode).parentNode).parentNode).parentNode;
+            let data_attr = delete_button.$el.getAttribute('data-id');
+            for (var i = 0; i < block_of_do.$el.childNodes.length; i++) {
+                if(block_of_do.$el.children[i] !== undefined){
+                    if(block_of_do.$el.children[i].getAttribute('data-id') === data_attr){
+                        console.log('YES');
+                        
+                        block_of_do.$el.children[i].remove();
+                    }
+                }
+                
+            }
+        })
     }
 })
 
 function getBlockOfDo(title){
-    return `<div class="col-md-12 to-do-block" >
+    return `<div class="col-md-12 to-do-block" data-id=${++id}>
         <div class="row">
             <div class="col-11">
                 <p class="m-0" id="title">${title}</p>
             </div>
             <div class="col-1 p-1">
-                <button class='delete-button'><i class="fa fa-times" aria-hidden="true"></i></button>
+                <button class="delete-button" id='delete_button' data-id=${id}><i class="fa fa-times" aria-hidden="true"></i></button>
             </div>
         </div>
     </div>`
 }
+
+
