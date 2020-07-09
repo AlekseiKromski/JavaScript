b55f22caf5e7adf97fd1eac44389745f287bd26b
AlekseiKrosmki <aleksei.kromski@ivkhk.ee>
2020-07-09 21:08:19 +0300

    learned fetch (ajax in default js)

---
+++ ajax/fetch.js
@@ -0,0 +1,16 @@
+fetch('http://getpost.itgid.info/index2.php',{
+    method: 'POST', // *GET, POST, PUT, DELETE, etc.
+    mode: 'cors', // no-cors, *cors, same-origin
+    cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
+    credentials: 'same-origin', // include, *same-origin, omit
+    headers: {
+      //'Content-Type': 'application/json'
+      'Content-Type': 'application/x-www-form-urlencoded',
+    },
+    redirect: 'follow', // manual, *follow, error
+    referrerPolicy: 'no-referrer', // no-referrer, *client
+    body: JSON.stringify(data) // body data type must match "Content-Type" header  
+}).then((response) => {
+    console.log(response);
+    
+})
