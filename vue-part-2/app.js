e0476cdefc7117a3469b32a1826b79ad7ed075c1
alekseiKromski <aleksei.kromski@ivkhk.ee>
2020-07-20 19:41:19 +0300

    Vue JS 2 #7 События клавиатуры (Keyboard Events)

--- vue-part-2/app.js
+++ vue-part-2/app.js
@@ -2,8 +2,19 @@ new Vue({
     //get dom access to vue.js
     el: "#app",
 
+    config:{
+        keyCodes:{
+            f2: 113
+        }
+    },
+
     //Filed for variables
     data: {
         counter: 0
+    },
+    methods:{
+        changeName(){
+            console.log('Name is changed!');
+        }
     }
 })
