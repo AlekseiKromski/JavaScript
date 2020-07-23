e25413a8a13112f08fc3c2e715a091a66e0ccbef
alekseiKromski <aleksei.kromski@ivkhk.ee>
2020-07-23 21:50:08 +0300

    Make to do with vue.js

--- vue-part-2/app.js
+++ vue-part-2/app.js
@@ -2,28 +2,51 @@ new Vue({
     //get dom access to vue.js
     el: "#app",
 
-    //Add our personal key shortcut
-    config:{
-        keyCodes:{
-            f2: 113
-        }
-    },
-
     //Filed for variables
     data: {
-        error:false,
-        success: false,
+        currentTask: '',
+        nextId: 3,
+        tasks: [
+            {id: 1, preview: '', isEditing: false, text: "Buy a dog",isComplete: false},
+            {id: 2, preview: '', isEditing: false, text: "Buy a fish",isComplete: false},
+            {id: 3, preview: '', isEditing: false, text: "Buy a jetBrains",isComplete: false}
+
+        ]
     },
     methods:{
+        addTask(){
+            this.nextId++;
+            this.tasks.push({
+                id: this.nextId,
+                text: this.currentTask,
+                isComplete: false,
+            })
+            this.currentTask = '';
+            console.log(this.tasks)
+        },
+        removeTask(id){
+            this.tasks = this.tasks.filter(task => {
+                return task.id !== id;
+            })
+        },
+        changeTask(id) {
+            this.tasks.forEach((task, i, tasks) => {
+                if (task.id === id) {
+                    task.isEditing = true;
+                }
+            })
+        },
+        hideEditingFields(task){
+            task.isEditing = false;
+        },
+        changeAllTask(taskText, task){
+            task.text = taskText;
+            task.isEditing = false;
+        }
+
     },
     computed:{
-        addtoA(){
-            return this.a + this.Age;
-        },
-        addtoB(){
-            return this.b + this.Age;
 
-        }
     }
 
 })
