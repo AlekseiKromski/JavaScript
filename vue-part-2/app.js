87a1c4e594d7ccbf1c7a25eb292a7972296982ca
alekseiKromski <aleksei.kromski@ivkhk.ee>
2020-07-23 22:26:25 +0300

    Vue JS 2 #14 Ссылки на элементы (Refs)

--- vue-part-2/app.js
+++ vue-part-2/app.js
@@ -1,27 +1,15 @@
-Vue.component('greet', {
-    template: '<h2>{{test}}<button @click="changeTitle">ChangeTitle</button></h2>',
-    data: function () {
-        return {
-            test: 'Hello from component'
-        }
-    },
-    methods: {
-        changeTitle(){
-            this.test = 'Updated title';
-        }
-    }
-});
-
 new Vue({
     //get dom access to vue.js
     el: "#app1",
 
     //Filed for variables
     data: {
-        title: 'Title in vue1'
+        title: 'title'
     },
     methods:{
-
+        changeTitle(){
+            this.title = this.$refs.input.value;
+        }
 
     },
     computed:{
