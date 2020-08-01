094d0155bb6833acf7f4ddb0ca73138befa704ae
alekseiKromski <aleksei.kromski@ivkhk.ee>
2020-08-01 17:11:27 +0300

    Vue JS 2 #26 Фильтры (Filters)

---
+++ vue-part-2/lesson25/src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div id="app">
+    <AddBlog></AddBlog>
+  </div>
+</template>
+
+<script>
+import List from './components/List';
+import Header from './components/Header';
+import AddBlog from './components/AddBlog';
+
+export default {
+  components: {
+    AddBlog
+  },
+  data() {
+    return {
+    }
+  },
+  methods: {
+    displayMessage: function(message) {
+      this.people.push(message)
+    }
+  }
+}
+</script>
---
+++ vue-part-2/lesson25/src/components/List.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+    <input type="text" v-model="message" @keyup.enter="saveMessage">
+    <h2>Inner title</h2>
+    <ul>
+      <li v-for="person in people">
+        {{person}}
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+import { bus } from '../main';
+
+export default {
+  props: {
+    people: {
+      type: Array,
+      required: false,
+      default: [],
+    }
+  },
+  data() {
+    return {
+      message: ''
+    }
+  },
+  methods: {
+    saveMessage: function() {
+      bus.$emit('message', this.message);
+      this.message = '';
+    }
+  }
+}
+</script>
