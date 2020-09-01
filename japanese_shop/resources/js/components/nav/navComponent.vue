b7c6543153c6f5d54bcf3950dca9033f0e7a305e
alekseiKromski <aleksei.kromski@ivkhk.ee>
2020-09-01 22:06:01 +0300

    make navbar + part of slider

---
+++ japanese_shop/resources/js/components/nav/navComponent.vue
@@ -0,0 +1,119 @@
+<template>
+    <div v-bind:class="navClass">
+        <div class="row justify-content-center" v-bind:style="rowStyle">
+            <div class="col-1 p-0 align-self-center menu">
+                Main
+            </div>
+            <div class="col-1 p-0 align-self-center menu">
+                Catalog
+            </div>
+            <div class="col-1 p-0 align-self-center menu">
+                News
+            </div>
+            <div class="col-2 p-0 align-self-center logo_block">
+                <img src="img/logo.png" alt="" class="logo_img" v-if="showLogo">
+                <span v-if="!showLogo" class="logo_text">JAPANESE SHOP</span>
+            </div>
+            <div class="col-1 p-0 align-self-center menu">
+                Shops
+            </div>
+            <div class="col-1 p-0 align-self-center menu">
+                About
+            </div>
+            <div class="col-1 p-0 align-self-center menu">
+                Contact
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    created(){
+        this.navStyle = {};
+    },
+    mounted(){
+        $(window).scroll(() => {
+            if ($(window).scrollTop() > 200) {
+                this.showLogo = false;
+                this.navClass.nav_img = false;
+                this.navClass.nav_text = true;
+                this.rowStyle["min-height"] = '100px';
+                this.navClass.animate__fadeInDown = true;
+            }else {
+                this.navClass.animate__fadeInDown = false;
+                this.showLogo = true;
+                this.navClass.nav_img = true;
+                this.navClass.nav_text = false;
+                this.rowStyle["min-height"] = '150px';
+
+            }
+        });
+    },
+    data(){
+        return {
+            showLogo: true,
+            navClass: {
+                'container-fluid': true,
+                'nav_img': true,
+                'animate__animated': true,
+                'animate__fadeInDown': false,
+                'nav_text': false,
+                'animate__fadeOutUp': false
+            },
+            rowStyle:{
+                'min-height': '150px',
+                'width': '100%'
+            }
+        }
+    }
+}
+</script>
+
+<style scoped>
+    .nav_img{
+        font-family: 'Advent Pro', sans-serif;
+        width: 100%;
+        min-height: 150px;
+        background: #242424;
+    }
+    .nav_text{
+        font-family: 'Advent Pro', sans-serif;
+        width: 100%;
+        min-height: 100px;
+        background: #242424;
+        position: fixed;
+        top: 0px;
+        z-index: 1;
+    }
+    .logo_img{
+        width: 100%;
+    }
+    .logo_text{
+        font-weight: 500;
+        font-size: 40px;
+        line-height: 48px;
+        background: -webkit-linear-gradient(#E21E42, rgba(226, 30, 66, 0));
+        -webkit-background-clip: text;
+        -webkit-text-fill-color: transparent;
+
+    }
+    .logo_block{
+        text-align: center;
+    }
+
+</style>
+<style>
+    .menu{
+        text-align: center;
+        color: white;
+        font-size: 26px;
+        padding: 0%;
+        margin: 0%;
+        transition: 0.5s;
+    }
+    .menu:hover{
+        color: #E21E42;
+        transition: 0.5s;
+    }
+</style>
---
+++ japanese_shop/resources/js/components/slider/sliderComponent.vue
@@ -0,0 +1,85 @@
+<template>
+    <div class="container-fluid block_slider p-0" v-bind:style="block_slider_style">
+        <div class="row justify-content-center" style="width: 100%">
+            <div class="col-10">
+                <div class="container-fluid slider" v-bind:style="sliderStyle">
+                    <div class="row justify-content-center">
+                        <div class="col-6 align-self-center">
+                            <h1>
+                                KATANA - Isiru<br>
+                                89.00$
+                            </h1>
+                            <p>
+                                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+name: "sliderComponent",
+    mounted() {
+        $(window).scroll(() => {
+            if ($(window).scrollTop() > 200) {
+                this.block_slider_style["margin-top"] = '420px';
+            }else {
+                this.block_slider_style["margin-top"] ='200px';
+            }
+        });
+    },
+    data(){
+        return{
+            sliderStyle: {
+                'background': 'linear-gradient(360deg, #000000 -52.61%, rgba(255, 255, 255, 0) 100%), url("img/bg.jpg")',
+                'background-size': 'cover',
+            },
+            block_slider_style:{
+                'margin-top': '200px'
+            }
+
+
+        }
+    }
+}
+</script>
+
+<style scoped>
+    .slider{
+        min-height: 593px;
+
+    }
+    .block_slider{
+        min-height: 1000px;
+
+    }
+
+</style>
+
+<style>
+    .row{
+        min-height: 593px;
+    }
+    .row h1{
+        font-family: Advent Pro;
+        font-style: normal;
+        font-weight: 500;
+        font-size: 38px;
+        line-height: 45px;
+
+        color: #FFFFFF;
+    }
+    .row p{
+        font-family: Advent Pro;
+        font-style: normal;
+        font-weight: 500;
+        font-size: 20px;
+        line-height: 24px;
+
+        color: #FFFFFF;
+    }
+</style>
