37cf3b6b1729b343f5f01b157c8e4ec34815aca6
alekseiKromski <aleksei.kromski@ivkhk.ee>
2020-09-02 22:32:22 +0300

    start repeat js basics

---
+++ js_basics/app.js
@@ -0,0 +1,18 @@
+//Приоброзование типов данных
+
+let value = "true";
+
+value = Boolean(value);
+
+console.log(typeof(value))
+
+//Приобразование строки в число унарным оператором
+
+let str1 = "2";
+
+console.log(typeof +str1);
+
+//пре/пост фиксная запись
+let c = 0
+alert(++c)
+
