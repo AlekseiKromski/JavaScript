72c8bf8f19669d8d9329c30f18ff320f8ad9b493
alekseiKromski <aleksei.kromski@ivkhk.ee>
2020-09-03 21:44:50 +0300

    функции

--- js_basics/app.js
+++ js_basics/app.js
@@ -77,3 +77,21 @@ alert(result)
 //     }
 // }
 
+// --- NAME: функции ---
+
+// function ask(question, yes, no) {
+//     if (confirm(question)) yes()
+//     else no();
+// }
+//
+// function showOk() {
+//     alert( "Вы согласны." );
+// }
+//
+// function showCancel() {
+//     alert( "Вы отменили выполнение." );
+// }
+//
+// // использование: функции showOk, showCancel передаются в качестве аргументов ask
+// ask("Вы согласны?", showOk, showCancel);
+
