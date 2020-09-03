d7f8c77ed9a9cf39075d40fa7775d26260b5362f
alekseiKromski <aleksei.kromski@ivkhk.ee>
2020-09-03 20:41:46 +0300

    alert, prompt, confirm

--- js_basics/app.js
+++ js_basics/app.js
@@ -1,3 +1,7 @@
+/*
+
+02.09.2020
+
 //Приоброзование типов данных
 
 let value = "true";
@@ -15,4 +19,15 @@ console.log(typeof +str1);
 //пре/пост фиксная запись
 let c = 0
 alert(++c)
+*/
 
+//alert, prompt, confirm
+
+// alert('Hello')
+
+// let result = prompt("How old are you?",100)
+// console.log(result)
+
+// let result = confirm('WHO MAIN&')
+// console.log(result)
+//Усоловный оператор if
