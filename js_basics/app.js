bac76a2ba501b48356fceb5e39c04f6719aa6b16
alekseiKromski <aleksei.kromski@ivkhk.ee>
2020-09-22 14:27:06 +0300

    замыкания

--- js_basics/app.js
+++ js_basics/app.js
@@ -1,6 +1,8 @@
 
-function sumAll2(a,b,s,c,s){
-    console.log(a,b,s);
+function createCalcFunc(n){
+    return function(){
+        console.log(1000 * n);
+    }
 }
-let arr = [1,2,3,4];
-sumAll2(...arr)
+
+createCalcFunc(42)()
