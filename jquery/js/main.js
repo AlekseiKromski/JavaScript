42784d972da9945314c29063bd808ad6797bcf59
alekseiKromski <aleksei.kromski@ivkhk.ee>
2020-09-23 22:44:20 +0300

    make form with jquery

--- jquery/js/main.js
+++ jquery/js/main.js
@@ -1,77 +1,44 @@
-jQuery(() => {
-    $(".first").css({'color': 'red'});
+$(document).ready(() => {
+    $('#thx').hide();
+    const input_email = $("#exampleInputEmail1");
+    const email_help = $("#emailHelp");
+    input_email.on("focus", (e) => {
 
-    //Data tags 
-    let data = $(".first").data('id');
-
-    //Attributes
-    // [] - для более строгой выборки css 
-    // Есть моножество различных способов выборки см. документацию
-    data = $("h1[title='test']").css({"color": "green"})
-
-    //Filter
-    data = $("ul li:nth-child(3)").css({"color": "green"})
-
-    //Если есть слово 'Test'
-    data = $("h1:contains('Test')").css({"color": "blue"})
-
-    //Проверка, существует ли такой элемент
-    data = $("ul li:has('span')").css({"color": "yellow"})
-
-    //Проверка, существует ли такой элемент
-    //data = $("ul li:parent").css({"color": "pink"})
-
-    //Если сущесвует конетен внутри тега
-    data = $("table tr td:parent").css({"background": "pink"})
-
-    //Если не сущесвует конетен внутри тега
-    data = $("table tr td:empty").css({"background": "black"})
-    //console.log(data);
-
-    //События
-    // $('button').click((event) => {
-    //     alert();
-    // })
-
-    //Click, dblclick, mouseenter, mouseleave
-    //keypress, keydown, keyup
-    //change
-    // $('input[name="name"]').change(() => {
-    //     $('input[name="submit"]').val(
-    //         $('input[name="name"]').val()
-    //     )
-    // })
-
-    //show, hide, delay - задержка, animate
-    //fideIn, fadeOut - non displat use 
-    $('form').show(1000).delay(2000).hide(1000)
-    $('#box').animate({'width': '50px'}, 2000)
-
-    //resize, scroll
-    // $(window).resize(() => {
-    //     //width and hight
-    //     var width = $(this).width();
-    //     console.log(width);
-    // })
-    // $(window).scroll(() => {
-    //     //width and hight
-    //     var height = $(this).height();
-    //     console.log(height);        
-    // })
-
-    //addClass amd removeClass
-    
-    // let text = $('p').text();
-    // let html = $('ul').html();
-
-    //Append and prepend 
-    //$('ul').append('<li>Test</li>')
-
-    // $('form').wrap("<div style=\"background: pink\">Test</div>")
-    // $('form').unwrap()
-
-    $('.slider').slick({
-        "setting-name": "setting-value"
+        input_email.on("keydown", (e) => {
+            let str = input_email.val();
+            if( str.length >= 8 ){;
+                email_help.addClass("animate__flash");
+                document.querySelector("#emailHelp").addEventListener('animationend', () => {
+                    email_help.removeClass("animate__flash");
+                });
+                if(e.keyCode != 8 && e.keyCode != 116)  e.preventDefault() ;
+            }
+        })
     });
 
+    $('form').submit((e) => {
+        e.preventDefault();
+        new Promise((resolve, reject) => {
+            $('form').animate({
+                opacity: 0
+            },500, () => {
+                $('form').hide();
+                $('.email-logo').css({display: "block"})
+                $('.logo').animate({
+                    opacity: 1
+                }, 1000, () => {
+                    resolve();
+                })
+            });
+        }).then(response => {
+            $('.form-block').addClass("animate__backOutUp");
+            document.querySelector(".form-block").addEventListener('animationend', () => {
+                $('.form-block').removeClass("animate__backOutUp");
+                $('form').hide();
+                $('#thx').show();
+            });
+        });
+        
+        
+    });
 });
