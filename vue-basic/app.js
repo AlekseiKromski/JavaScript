037706d5e86d2be968b1f956d426c22d778dc97f
alekseiKromski <aleksei.kromski@ivkhk.ee>
2020-10-07 20:37:35 +0300

    Vue JS 2 #10 Условный рендеринг (Conditionals)

--- vue-basic/app.js
+++ vue-basic/app.js
@@ -1,17 +1,10 @@
 new Vue({
     el: "#app",
     data: {
-        a: 0,
-        b: 0,
-        age: 20
+        error:  false,
+        success: false, 
 
     },
     computed:{
-        addToA: function(){
-            return this.a + this.age;
-        },
-        addToB: function(){
-            return this.b + this.age;
-        },
     }
 });
