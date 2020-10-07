dff9f59d10fd2eaf2914c5a91d953c03571c1869
alekseiKromski <aleksei.kromski@ivkhk.ee>
2020-10-07 20:05:10 +0300

    Vue JS 2 #5 События (Events)

--- vue-basic/app.js
+++ vue-basic/app.js
@@ -1,23 +1,7 @@
 new Vue({
     el: "#app",
     data: {
-        people: [
-            {
-                name: "Franky Four Fingers",
-                stillAlive: false
-            },
-            {
-                name: "Franky Four Fingers 1",
-                stillAlive: true
-            },
-            {
-                name: "Franky Four Fingers 2",
-                stillAlive: false
-            },
-            {
-                name: "Franky Four Fingers 3",
-                stillAlive: true
-            }
-        ]
+        counter: 0,
+        
     },
 });
