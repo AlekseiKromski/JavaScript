0ecbb16830fb6066908ef25e168de86bc0c53093
alekseiKromski <aleksei.kromski@ivkhk.ee>
2020-10-07 20:16:47 +0300

    Vue JS 2 #7 События клавиатуры (Keyboard Events)

--- vue-basic/app.js
+++ vue-basic/app.js
@@ -2,6 +2,11 @@ new Vue({
     el: "#app",
     data: {
         counter: 0,
-        
+
     },
+    methods:{
+        change: function (){
+            console.log("Change");
+        }
+    }
 });
