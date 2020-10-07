53d1589c5942e71d5219738d0af263843fd68b8d
alekseiKromski <aleksei.kromski@ivkhk.ee>
2020-10-07 20:31:06 +0300

    Vue JS 2 #9 Вычисляемые свойства (Computed Properties)

--- vue-basic/app.js
+++ vue-basic/app.js
@@ -1,11 +1,17 @@
 new Vue({
     el: "#app",
     data: {
-        name: "",
-        checked: false
+        a: 0,
+        b: 0,
+        age: 20
 
     },
-    methods:{
-        
+    computed:{
+        addToA: function(){
+            return this.a + this.age;
+        },
+        addToB: function(){
+            return this.b + this.age;
+        },
     }
 });
