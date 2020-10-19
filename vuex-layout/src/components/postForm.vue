b164837e047a7d4652b16faa8d0af18a34a3517c
alekseiKromski <aleksei.kromski@ivkhk.ee>
2020-10-19 11:08:06 +0300

    start learn vuex

---
+++ vuex-layout/src/components/postForm.vue
@@ -0,0 +1,30 @@
+<template>
+  <form @submit.prevent="submit" action="">
+      <input type="text" v-model="title">
+      <input type="submit" value="make post">
+  </form>
+</template>
+
+<script>
+import {mapMutations} from 'vuex'
+export default {
+    data: function(){
+        return {
+            title: ""
+        }
+    },
+    methods:{
+        ...mapMutations(['createPost']),
+        submit: function(){
+            this.createPost({
+                id: Date.now(),
+                title: this.title,
+            });
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
