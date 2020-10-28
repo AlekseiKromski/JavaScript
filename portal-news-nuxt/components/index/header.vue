06625b30e3a9e80201bf7113d80110782ff20a7c
alekseiKromski <aleksei.kromski@ivkhk.ee>
2020-10-28 20:18:48 +0200

    clear project

--- portal-news-nuxt/components/index/header.vue
+++ portal-news-nuxt/components/index/header.vue
@@ -1,22 +1,16 @@
 <template>
     <div class="container-fluid header">
         <div class="row justify-content-center">
-            <div class="col-6" v-for="news in topNews" v-bind:key="news.id">
-                <h3>Top</h3>
-            </div>    
+             
         </div>    
     </div> 
 </template>
 
 <script>
 export default {
-    props:['topNews'],
     computed:{
         
     },
-    mounted(){
-        console.log(this.topNews);
-    }
 }
 </script>
 
--- portal-news-nuxt/pages/index.vue
+++ portal-news-nuxt/pages/index.vue
@@ -1,34 +1,16 @@
 <template>
-  <headerComponent topNews="getTopNews"></headerComponent>
+  <headerComponent></headerComponent>
 </template>
 
 <script>
 import header from '@/components/index/header';
 import { mapMutations } from 'vuex';
-import {mapGetters} from 'vuex';
+import { mapGetters } from 'vuex';
 export default {
   layout: 'Default',
   components: {
     headerComponent: header,
 
-  },
-  computed:{
-    ...mapGetters({
-      getTopNews: 'news/getTopNews'
-    })
-  },
-  methods:{
-    ...mapMutations({
-      setNews: 'news/setNews'
-    })
-  },
-  async created(){
-    this.$axios.get('https://jsonplaceholder.typicode.com/posts').then(response => {
-      this.setNews(response)
-    })
-  },
-  mounted(){
-    console.log(getTopNews);
   }
 }
 </script>
