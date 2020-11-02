224bfe4e8e1bd9d295a58b149b9a8c5868344109
alekseiKromski <aleksei.kromski@ivkhk.ee>
2020-11-02 19:10:09 +0200

    make nav toogle bar in vue

---
+++ fuel/src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="app">
+    <navBar></navBar> 
+    <div class="container-fluid">
+      <div class="row">
+        <div class="col-4">
+          <consumption></consumption> 
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import nav from './components/navigation'
+import consumption from './components/app.components/consumption'
+
+export default {
+  name: 'App',
+  components: {
+    navBar: nav,
+    consumption: consumption
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ fuel/src/components/app.components/consumption.vue
@@ -0,0 +1,80 @@
+<template>
+    <div class="row shadow rounded">
+            <div class="col-12 p-5">
+                <ul class="nav nav-tabs">
+                    <li class="nav-item" v-for="link in links" v-bind:key="link.id">
+                        <!-- V-IF -->
+                        <a class="nav-link"
+                        v-bind:class="{active : link.active}"
+                        v-bind:data-id="link.id"
+                        v-on:click="changeNav($event, link)"
+                        >{{link.name}}</a>
+                    </li>
+                </ul>
+                <div class="tab-content" id="myTabContent">
+                    <div v-for="link in links" v-bind:key="link.id" >
+                        <div v-if="link.active" v-html="link.content"></div>
+                    </div>
+                </div>
+            </div>
+        </div>
+</template>
+
+<script>
+
+export default {
+    data: function(){
+        return {
+            links:[
+                {
+                    id: "howMuchFuel",
+                    name: "fuel",
+                    active: true,
+                    content: `
+                    <h1>Test1</h1>
+                    <form class="mt-3">
+                            <div class="input-group mb-3">
+                                <div class="input-group-prepend">
+                                    <span class="input-group-text" id="inputGroup-sizing-default">Default</span>
+                                </div>
+                                <input type="text" class="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-default">
+                                <input type="text" class="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-default">
+                            </div>
+                        </form>`
+
+                },
+                {
+                    id: "howMuchKM",
+                    name: "km",
+                    active: false,
+                    content: `
+                    <h1>Test2</h1>
+                    <form class="mt-3">
+                            <div class="input-group mb-3">
+                                <div class="input-group-prepend">
+                                    <span class="input-group-text" id="inputGroup-sizing-default">Default</span>
+                                </div>
+                                <input type="text" class="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-default">
+                                <input type="text" class="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-default">
+                            </div>
+                        </form>`
+
+                }
+            ]
+        }
+    },
+    methods:{
+        changeNav: function(e,item){
+            e.preventDefault();
+            this.links.forEach(e => {
+                if(e.active) e.active=false;
+            })
+            item.active = true;
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
