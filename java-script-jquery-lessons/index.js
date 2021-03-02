a68170b48ae8a8ab73553f27a42a4c702947a96b
alekseiKromski <aleksei.kromski@ivkhk.ee>
2021-03-02 18:16:05 +0200

    Start learn jquery

---
+++ java-script-jquery-lessons/index.js
@@ -0,0 +1,12 @@
+$(document).ready(function () {
+    var header = $('.header');
+    var a = $('<h2>Hello world</h2>');
+
+    //Add a element to end of header elemenet
+    header.append(a);
+})
+
+//Will call, when document will be ready
+$(function (){
+
+})
