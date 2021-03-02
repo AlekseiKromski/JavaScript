1391933362f008726c48ea1186bb4a583a6b0507
alekseiKromski <aleksei.kromski@ivkhk.ee>
2021-03-02 16:18:11 +0200

    strings

--- java-script-basics/index.js
+++ java-script-basics/index.js
@@ -28,5 +28,43 @@ console.log(typeof func);
 
 
 console.log(mystr.toUpperCase());
-*/
-console.log();
+
+var string = 'I love go to zoo, bc monkey is my monkey favorite animal';
+
+//Возвращает букву по индексу
+console.log(string.length);
+console.log(string.charAt(string.length - 1));
+
+//Получаем новую подстроку в указанном дипазоне
+//от указанного символа и до конца
+console.log("substring with 1 attribute - ",string.substring(22));
+
+//Указываем диапозон и получаем строчку
+console.log("substring with 2 attribute - ",string.substring(22,66));
+
+//Может иметь отрецательное значение
+//от указанного символа и начала (в обратную сторону)
+console.log("slice with 1 attribute - ", string.slice(20));
+
+//можем указать с какого символа и сколько символов надо взять
+console.log(string.substr(19, 5));
+
+//Return index of substring (Start search substing by 0)
+console.log(string.indexOf('monkey'));
+
+//Return index of substring (Start search substing by end of string)
+console.log(string.lastIndexOf('monkey'));
+
+//Can search and replace string (return new string)
+console.log(string.replace('love', 'like'));
+
+//Can make array by sparator
+console.log(string.split(','));
+
+//Change case
+console.log(string.toUpperCase());
+console.log(string.toLowerCase());
+
+//ES5 - строки могу интерпритироваться, как массивы
+//ES5 стандарт работает во всех современных браузерах
+console.log(string[13]);*/
