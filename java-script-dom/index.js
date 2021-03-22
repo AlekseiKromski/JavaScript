9af61c61555a4f33ad6a9bd063b13f7801d27e6d
alekseiKromski <aleksei.kromski@ivkhk.ee>
2021-03-22 21:16:15 +0200

    Classes and attributes

--- java-script-dom/index.js
+++ java-script-dom/index.js
@@ -2,3 +2,15 @@ var a = document.querySelector('a');
 var link = a.getAttribute('href');
 a.setAttribute('href', 'https://ya.ru')
 a.textContent = 'yandex'
+
+var box1 = document.querySelector('#box1');
+box1.classList.add('red')
+
+var box2 = document.querySelector('#box2');
+box2.classList.add('red')
+
+var hasClass = box2.classList.contains('blue');
+
+
+hasClass === true ? box2.classList.remove('blue') : box2.classList.add('blue')
+
