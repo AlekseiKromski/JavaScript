d63d275c156f9bb7bf6ddb9e86dbc281ad35957b
alekseiKromski <aleksei.kromski@ivkhk.ee>
2021-03-22 20:46:46 +0200

    dom

---
+++ java-script-dom/index.js
@@ -0,0 +1,10 @@
+//Old
+var palyground = document.getElementById('playground')
+var p = document.getElementsByClassName('test')
+var h1= document.getElementsByTagName('h1')
+
+
+//New style
+palyground = document.querySelector('#playground') 
+p = document.querySelectorAll('.test')
+console.log(p);
