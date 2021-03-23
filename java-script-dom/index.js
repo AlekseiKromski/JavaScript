03b6bb635231fa931aef2803db5167dae16acd0f
alekseiKromski <aleksei.kromski@ivkhk.ee>
2021-03-23 22:35:47 +0200

    Делегирование событий

--- java-script-dom/index.js
+++ java-script-dom/index.js
@@ -1,19 +1,13 @@
-var boxs = document.querySelectorAll('div');
+// var ps = document.querySelectorAll('p');
+// ps.forEach(e => {
+//     e.style.cursor = 'pointer'
+//     e.addEventListener('click', function (event) {
+//         event.target.style.color = 'blue'
+//     })
+// });
 
-boxs.forEach(e => {
-    e.addEventListener('click', function(event){
-        event.stopPropagation();
-        console.log(e.getAttribute('id'));
-    })
-})
-
-var toggler = document.querySelector('#toggle');
-var box2 = document.querySelector('.box2');
-toggler.addEventListener('click' , function(event){
-    event.preventDefault();
-    if(box2.classList.contains('hide')){
-        box2.classList.remove('hide')
-    }else{
-        box2.classList.add('hide')
+document.getElementById('wrapper').addEventListener('click', function(event){
+    if(event.target.tagName === 'P'){
+        event.target.style.color = 'blue'
     }
 })
