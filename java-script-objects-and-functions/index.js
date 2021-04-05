63ec395c7b91a081e65f186bc267d5e146e48ca8
alekseiKromski <aleksei.kromski@ivkhk.ee>
2021-04-05 19:50:30 +0300

    Итерация ключей объекта

--- java-script-objects-and-functions/index.js
+++ java-script-objects-and-functions/index.js
@@ -1,51 +1,11 @@
-var ford = Object.create({
-    calculateDistancePerYear: function(){
-        Object.defineProperty(this, 'distancePerYear', {
-            value: Math.floor(this.distance / this.age),
-            enumerable: true,
-            writable: false,
-            configurable: false,    
-        });
-    }
-}, {
-    name: {
-        value: 'Ford',
-        enumerable: true,
-        writable: false,
-        configurable: false,
-    },
-    model:{
-        value: 'focus',
-        enumerable: true,
-        writable: false,
-        configurable: false,
-    },
-    year: {
-        value: 2015,
-        enumerable: true,
-        writable: false,
-        configurable: false,
-    },
-    distance: {
-        value: 20000,
-        enumerable: true,
-        writable: true,
-        configurable: false,
-    },
-    age: {
-        set: function(age){
-            console.log('Setup value of age');
-        },
-        get: function(){
-            return new Date().getFullYear() - this.year
-        }
-    }
-})
-
-for(var key in ford){
-    if(ford.hasOwnProperty(key)){
-        console.log(key);
-
-    }
+var person = {
+    name: 'max',
+    age: 22,
+    job: 'front-end'
 }
 
+
+console.log(Object.keys(person));
+Object.keys(person).forEach(element => {
+    console.log(person[element]);
+})
