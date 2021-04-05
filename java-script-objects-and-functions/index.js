62392bcba2f60a91ad6a5ae33e13bcd8d3d94b80
alekseiKromski <aleksei.kromski@ivkhk.ee>
2021-04-05 20:18:37 +0300

    Привязка контекста

--- java-script-objects-and-functions/index.js
+++ java-script-objects-and-functions/index.js
@@ -1,11 +1,27 @@
-var person = {
-    age: 22,
-    name: 'Fedor',
-    job: 'JS',
-    display: function(time){
-        setTimeout(function(){
-            console.log(this.age, this.name, this.job);
-        }.bind(this), time)
+function printObject(objName) {
+    console.log(objName);
+    for(var key in this){
+        if(this.hasOwnProperty(key)){
+            console.log(`[${key}]`, this[key]);
+        }
     }
+}   
+
+var person = {
+    firstName: 'max',
+    job: 'js',
+    age: 29,
+    friends: [
+        'Elena', 'Igor'
+    ]
 }
-person.display(1000)
+
+var car = {
+    name: 'ford',
+    year: 2012,
+    model: 'focus'
+}
+
+printObject.call(car, 'car')
+console.log('++++++++++++++++');
+printObject.apply(person, ['person'])
