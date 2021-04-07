d3ed610193b6cafe3a0f1ab852e4dd8fd512bb7d
alekseiKromski <aleksei.kromski@ivkhk.ee>
2021-04-07 22:15:00 +0300

    Context

--- java-script-objects-and-functions/index.js
+++ java-script-objects-and-functions/index.js
@@ -1,6 +1,41 @@
-for(var i = 0; i < 5; i++){
-    setTimeout(function(){
-        console.log('ok');
-    },2000)
+// function getAge1(year){
+//     return new Date().getFullYear() - year;
+// }
+
+// console.log(getAge1(1937));
+
+
+// const getAge2 = (year) => {
+//     return new Date().getFullYear() - year;
+// }
+
+
+// console.log(getAge2(1937));
+
+
+// const getAge = year => {
+//     console.log(year);
+// }
+
+// getAge(12)
+
+// const getAge = year => new Date().getFullYear() - year;
+
+// console.log(getAge(2012));
+
+let a = 'Hello World!';
+
+function first() {
+  console.log('Inside first function');
+  var ss = 'test'
+  second();
+  console.log('Again inside first function');
+  function second() {
+    console.log(ss);
+  console.log('Inside second function');
 }
-console.log(i);
+}
+
+first();
+console.log('Inside Global Execution Context');
+
