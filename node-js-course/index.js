1c060cc7380ea691b96241f396286aa96f31ce75
alekseiKromski <aleksei.kromski@ivkhk.ee>
2021-04-29 23:31:01 +0300

    add scripts

--- node-js-course/index.js
+++ node-js-course/index.js
@@ -3,6 +3,7 @@ const http = require('http');
 const server = http.createServer((request, response) => {
     console.log(request.url);
     response.write('<h1>Hello from node js</h1>');
+    response.write('<h2>Test</h2>');
     response.end();
 })
 
