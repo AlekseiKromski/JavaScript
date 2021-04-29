00961b99fbc9b77ffc8ea99356b5ee9a60aa20c6
alekseiKromski <aleksei.kromski@ivkhk.ee>
2021-04-29 23:46:17 +0300

    simple web server

--- node-js-course/index.js
+++ node-js-course/index.js
@@ -1,10 +1,29 @@
 const http = require('http');
 
 const server = http.createServer((request, response) => {
-    console.log(request.url);
-    response.write('<h1>Hello from node js</h1>');
-    response.write('<h2>Test</h2>');
-    response.end();
+    response.writeHead(200, {
+        "Content-Type": 'text/html; charset=utf-8',
+    });
+    if(request.method === "GET"){
+        response.end(`
+            <h1>From</h1>
+            <form method="post" action="/">
+                <input name="title" type="text" />
+                <button type="submit">Отправить</button>
+            </form>
+        `)
+    }else if(request.method === "POST"){
+        const body = [];
+        request.on('data', data => {
+            body.push(Buffer.from(data));
+        });
+        request.on('end', () => {
+            const message = body.toString().split('=')[1]
+            response.end(`
+            <p>Your text is: ${message}</p>
+            `)
+        })
+    }
 })
 
 
