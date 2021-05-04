a143f713753e573202598cafdf60e51c9a0a3ea8
alekseiKromski <aleksei.kromski@ivkhk.ee>
2021-05-04 16:03:46 +0300

    Работа с HTML-файлами с express

--- node-js-express/index.js
+++ node-js-express/index.js
@@ -1,7 +1,19 @@
 const express = require('express');
 const app = express();
-const PORT = process.env.PORT || 3000;
+const path = require('path');
 
+
+app.get('/', (request, response) => {
+    response.status(200);
+    response.sendFile(path.join(__dirname, "views", "index.html"))
+});
+app.get('/about', (request, response) => {
+    response.status(200);
+    response.sendFile(path.join(__dirname, "views", "about.html"))
+});
+
+
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
     console.log('Server started on port: ' + PORT);
 })
