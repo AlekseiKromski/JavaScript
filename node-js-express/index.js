23c5b0a504e1c6365d372c730b8deb8a83c27ced
alekseiKromski <aleksei.kromski@ivkhk.ee>
2021-05-11 15:41:30 +0300

    Подключение Handlebars

--- node-js-express/index.js
+++ node-js-express/index.js
@@ -1,18 +1,31 @@
 const express = require('express');
+const exphbs = require('express-handlebars');
 const app = express();
-const path = require('path');
+
+const hbs = exphbs.create({
+    defaultLayout: 'main',
+    extname: 'hbs',
+});
+
+//Register hbs engine
+app.engine('hbs', hbs.engine);
+
+//SET to use hbs extension
+app.set('view engine', 'hbs');
+
+//Set default views path
+app.set('views', 'views');
+
 
 
 app.get('/', (request, response) => {
     response.status(200);
-    response.sendFile(path.join(__dirname, "views", "index.html"))
+    response.render('index');
 });
 app.get('/about', (request, response) => {
     response.status(200);
-    response.sendFile(path.join(__dirname, "views", "about.html"))
+    response.render('about');
 });
-
-
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
     console.log('Server started on port: ' + PORT);
